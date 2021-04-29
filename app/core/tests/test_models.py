a3c780ed98c912e768915a9cdb26486e1ccd2d96
Siddharth Mudgal <siddharthmudgal@Siddharths-MacBook-Pro-2.local>
2021-04-30 00:32:46 +0800

    ADDED superuser creation flow

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email address is success"""
+        email = 'test@tru.com'
+        password = 'testPassword123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """test that the email is normalized for a new user"""
+        email = 'test@TRU.com'
+        user = get_user_model().objects.create_user(email, 'testpassword')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """validate the email of a to be created user"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                None,
+                'testPassword123'
+            )
+
+    def test_create_new_super_user(self):
+        """Test creating a new super user"""
+        user = get_user_model().objects.create_superuser(
+            'test@tru.com',
+            'testPassword123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
