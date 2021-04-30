b9500d4eab34a72e8f877ca4f0593a335e5b3fd7
Siddharth Mudgal <siddharthmudgal@Siddharths-MacBook-Pro-2.local>
2021-05-01 02:20:52 +0800

    ADDED tag & ingredient filter based on if used

--- app/recipe/tests/test_ingredient_api.py
+++ app/recipe/tests/test_ingredient_api.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Ingredient
+from core.models import Ingredient, Recipe
 
 from recipe.serializers import IngredientSerializer
 
@@ -91,3 +91,54 @@ class PrivateIngredientsAPITest(TestCase):
         res = self.client.post(INGREDIENTS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_ingredients_assigned_to_recipes(self):
+        """test filtering tags by those assigend to reci"""
+        ing1 = Ingredient.objects.create(user=self.user, name='Breakfast')
+        ing2 = Ingredient.objects.create(user=self.user, name='Lunch')
+
+        recipe = Recipe.objects.create(
+            title='Coriander eggs on toast',
+            time_minutes=10,
+            price=5.00,
+            user=self.user
+        )
+        recipe.ingredients.add(ing1)
+
+        res = self.client.get(INGREDIENTS_URL, {'assigned_only': 1})
+
+        serializer1 = IngredientSerializer(ing1)
+        serializer2 = IngredientSerializer(ing2)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertNotIn(serializer2.data, res.data)
+
+    def test_retrieve_tags_assigned_unique(self):
+        """tags returned should be unique"""
+        ing1 = Ingredient.objects.create(user=self.user, name='Breakfast')
+        ing2 = Ingredient.objects.create(user=self.user, name='Lunch')
+
+        recipe1 = Recipe.objects.create(
+            title='Coriander eggs on toast',
+            time_minutes=10,
+            price=5.00,
+            user=self.user
+        )
+        recipe1.ingredients.add(ing1)
+
+        recipe2 = Recipe.objects.create(
+            title='Coriander on toast',
+            time_minutes=10,
+            price=5.00,
+            user=self.user
+        )
+        recipe2.ingredients.add(ing1)
+
+        res = self.client.get(INGREDIENTS_URL, {'assigned_only': 1})
+
+        serializer1 = IngredientSerializer(ing1)
+        serializer2 = IngredientSerializer(ing2)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertEqual(len(res.data), 1)
+        self.assertNotIn(serializer2.data, res.data)
