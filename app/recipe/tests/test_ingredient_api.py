19945cbc8ff09727b54210e281f656acacf9bba5
Siddharth Mudgal <siddharthmudgal@Siddharths-MacBook-Pro-2.local>
2021-04-30 19:25:13 +0800

    ADDED ingredient feature

--- app/recipe/tests/test_ingredient_api.py
+++ app/recipe/tests/test_ingredient_api.py
@@ -67,3 +67,27 @@ class PrivateIngredientsAPITest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ing.name)
+
+    def test_create_ingredients(self):
+        """test that authenticated user is able to create ingredient"""
+        payload = {
+            'name': 'Chilli'
+        }
+        res = self.client.post(INGREDIENTS_URL, payload)
+
+        exists = Ingredient.objects.all().filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(exists)
+
+    def test_create_ingredient_with_invalid(self):
+        """test that ingredient creation fails with invalid details"""
+        payload = {
+            'name': ''
+        }
+        res = self.client.post(INGREDIENTS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -25,7 +25,8 @@ class TagViewSet(viewsets.GenericViewSet,
 
 
 class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin):
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
     """Manage ingredient in database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -35,3 +36,6 @@ class IngredientViewSet(viewsets.GenericViewSet,
     def get_queryset(self):
         """return ingredient"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
